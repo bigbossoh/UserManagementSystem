a28d2887a78d3f046868ad6f2126f397d307880a
bigbossoh <michel.bossoh@outlook.fr>
2022-03-22 19:42:00 +0000

    Web Application Security video: 25. Java JWT Library

---
+++ src/main/java/com/bzdata/usermanagementsystem/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.bzdata.usermanagementsystem.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME=432_000_000;
+    public static final String TOKEN_PREFIX="Bearer ";
+    public static final String JWT_TOKEN_HEADER="Jwt-Token";
+    public static final String TOKEN_CANOT_BE_VERIFIED="Token cannot be verified";
+    public static final String BZDATA_SARL="BZDATA, Sarl";
+    public static final String BZDATA_ADMINISTRATION="User Management System";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = { "/ums/api/v1/user/login", "**/user/register", "**/user/image/**" };
+}
