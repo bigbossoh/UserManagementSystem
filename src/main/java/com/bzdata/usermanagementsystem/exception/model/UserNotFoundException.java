01797fd319a7bfef86a98701345ddf5019e65e94
bigbossoh <michel.bossoh@outlook.fr>
2022-04-04 16:54:13 +0000

    We have done with the section 5 we are going to the section 6 which are User Registration

---
+++ src/main/java/com/bzdata/usermanagementsystem/exception/model/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bzdata.usermanagementsystem.exception.model;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
